6f54258defa91f5a8f209c7898fc7e295eda3898
AhmetEsad <38183175+AhmetEsad@users.noreply.github.com>
2020-03-08 12:03:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const bitchat = require('./index');
+const client = new bitchat.client();
+
+var prefix = '!';
+
+client.on('ready', function () {
+    client.login('username', 'password');
+    client.switchChannel('general');
+});
+
+client.on('message', function (msg) {
+    if(msg.content == prefix + 'hello') {
+        client.send('Hi, ' + msg.author.username + '!');
+    }
+});
+
+client.on('announcement', function(msg) {
+    console.log('> ' + msg);
+});
+
+client.on('disconnect', function() {
+    console.log('Disconnected');
+    process.exit();
+});
