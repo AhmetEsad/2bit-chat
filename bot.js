50b15ba8fc40980271739b9c189549c1fc1595fc
AhmetEsad <38183175+AhmetEsad@users.noreply.github.com>
2020-04-14 01:07:16 +0300

    Promises?

--- bot.js
+++ bot.js
@@ -1,24 +1,28 @@
-const bitchat = require('./index');
+const bitchat = require('./events');
 const client = new bitchat.client();
 
 var prefix = '!';
 
 client.on('ready', function () {
-    client.login('username', 'password');
-    client.switchToChannel('general');
+    client.login('username', 'password').then(function (msg) { console.log(msg) }).catch((response) => { console.log(response) });
+    client.switchToChannel('general'); // no promises for this function, sorry :(
 });
 
-client.on('message', function (msg) {
-    if(msg.content == prefix + 'hello') {
-        client.send('Hi, ' + msg.author.username + '!');
+client.on('message', async function (msg) {
+    console.log(msg.author.badges.map(b => b.emoji).join(' ') + ' ' + msg.author.username + ': ' + msg.content);
+    if (msg.content === prefix + 'hello') {
+        var myMessage = await client.send('Hi, ' + msg.author.username + '!');
+        console.log(myMessage.id);
     }
 });
 
-client.on('announcement', function(msg) {
+client.on('system', function (msg) { });
+
+client.on('announcement', function (msg) {
     console.log('> ' + msg);
 });
 
-client.on('disconnect', function() {
+client.on('disconnect', function () {
     console.log('Disconnected');
     process.exit();
 });
